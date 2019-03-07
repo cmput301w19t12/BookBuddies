a4f16f94e948e8bacb7f6f5db36bf661aa9fbad5
bgreni <42788181+bgreni@users.noreply.github.com>
2019-03-07 00:39:58 -0700

    Take new book photos|get photos from gallery|current photo put in database

--- app/src/main/java/com/cmput301w19t12/bookbuddies/NewBookActivity.java
+++ app/src/main/java/com/cmput301w19t12/bookbuddies/NewBookActivity.java
@@ -1,15 +1,19 @@
 package com.cmput301w19t12.bookbuddies;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -26,13 +30,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -63,6 +71,7 @@ public class NewBookActivity extends AppCompatActivity implements PopupMenu.OnMe
     private static final int SELECT_PICTURE = 1;
     private String selectedImagePath;
     private String fileManagerString;
+    private String currentPhotoPath;
 
     private static final int REQUEST_IMAGE_CAPTURE = 2;
 
@@ -91,13 +100,19 @@ public class NewBookActivity extends AppCompatActivity implements PopupMenu.OnMe
             @Override
             public void onClick(View v) {
                 addBookToDatabase();
+
             }
         });
 
         editImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showMenu(v);
+                if(ISBNField.getText().toString().isEmpty()){
+                    ISBNField.setError("Must add ISBN before adding photo");
+                }
+                else {
+                    showMenu(v);
+                }
             }
         });
 
@@ -129,71 +144,91 @@ public class NewBookActivity extends AppCompatActivity implements PopupMenu.OnMe
    }
 
 
-   private void takePicture(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+   private void takePicture() {
+       Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        /*if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("File Error", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            }
+        }*/
        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-           // Create the File where the photo should go
-           File photoFile = null;
-           try {
-               photoFile = createImageFile();
-           } catch (IOException ex) {
-               // Error occurred while creating the File
-               Log.e("File Error","Error occured while creating image file");
-           }
-           // Continue only if the File was successfully created
-           if (photoFile != null) {
-               Uri photoURI = FileProvider.getUriForFile(this,
-                       "com.example.android.fileprovider",
-                       photoFile);
-               takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-               startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
-           }
+           startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
        }
-   }
 
-   private File createImageFile() throws IOException{
-        String currentPhotoPath;
-       // Create an image file name
-       String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-       String imageFileName = "JPEG_" + timeStamp + "_";
-       File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-       File image = File.createTempFile(
-               imageFileName,  /* prefix */
-               ".jpg",         /* suffix */
-               storageDir      /* directory */
-       );
-
-       // Save a file: path for use with ACTION_VIEW intents
-       currentPhotoPath = image.getAbsolutePath();
-       return image;
    }
 
 
 
-   private void getImageFromStorage(){
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select Picture"),SELECT_PICTURE);
-   }
+
 
    @Override
    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
         Log.i("STUFF","STUFF");
-        if(requestCode == SELECT_PICTURE){
+        if(requestCode == SELECT_PICTURE && resultCode == RESULT_OK){
             Uri selectedImageUri = data.getData();
-            fileManagerString = getPath(selectedImageUri);
-            Log.i("STUFF","STUFF"+fileManagerString);
+            ActivityCompat.requestPermissions(NewBookActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},124);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                bookImage.setImageBitmap(bitmap);
+                addPhotoToDatabase();
+            }
+            catch (Exception e){
+                Log.e("Image Load Error",e.getMessage());
+            }
         }
-        else if(requestCode == REQUEST_IMAGE_CAPTURE){
+        else if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap imageBitMap = (Bitmap) extras.get("data");
             bookImage.setImageBitmap(imageBitMap);
-
+            addPhotoToDatabase();
+            //galleryAddPic();
         }
    }
 
+   private void addPhotoToDatabase(){
+       Bitmap bitmap = ((BitmapDrawable) bookImage.getDrawable()).getBitmap();
+       ByteArrayOutputStream baos = new ByteArrayOutputStream();
+       bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+       byte[] data = baos.toByteArray();
+
+       UploadTask uploadTask = mStorageRef.child("images").child(ISBNField.getText().toString()).putBytes(data);
+       uploadTask.addOnFailureListener(new OnFailureListener() {
+           @Override
+           public void onFailure(@NonNull Exception exception) {
+               // Handle unsuccessful uploads
+           }
+       }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+           @Override
+           public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+               // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+               // ...
+           }
+       });
+   }
+
+
+    private void getImageFromStorage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"),SELECT_PICTURE);
+    }
 
     public String getPath(Uri uri) {
         // just some safety built in

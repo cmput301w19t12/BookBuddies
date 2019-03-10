dcd9301ab69ed179059af819323b2a5384e0f240
unknown <junhong1@ualberta.ca>
2019-03-09 20:19:16 -0700

    Club page

---
+++ app/src/main/java/com/cmput301w19t12/bookbuddies/ClubPageActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301w19t12.bookbuddies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClubPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_page);
+    }
+}
